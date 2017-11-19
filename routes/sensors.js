e823e250ccfcce3038c74a15c7883135ab957a9e
Felipe Nogueira <fadsnogueira@gmail.com>
2017-11-19 00:36:55 -0200

    Add validation to sensors post

--- routes/sensors.js
+++ routes/sensors.js
@@ -4,10 +4,32 @@ var db = require("../db");
 
 /* Insert sensor notification in database */
 router.post('/', function(req, res) {
-	console.log("Receive POST: " + req.body);
+	console.log("Receive POST to /sensors: " + req.body);
 
 	var type =  req.body.type;
 	var info = req.body.info;
+
+	//Validate values
+	if(type == "door") {
+		if (info !== "main" && info !== "medicine") {
+			res.status(400);
+			res.send({ error: "Invalid info for the type door"});
+			return;
+		}
+	} else if (type == "passage") {
+		if (info !== "main" && info !== "hall") {
+			res.status(400);
+			res.send({ error: "Invalid info for the type passage"});
+			return;
+		}
+	} else if (type == "moviment") {
+		if (info !== "moving" && info !== "fall") {
+			res.status(400);
+			res.send({ error: "Invalid info for the type moviment"});
+			return;
+		}
+	}
+
 	var Sensors = db.Mongoose.model('sensors', db.SensorSchema, 'sensors');
 	var sensor = new Sensors({ type: type, info: info });
 	sensor.save(function (err) {
@@ -20,7 +42,7 @@ router.post('/', function(req, res) {
 			res.status(200);
 			res.send(req.body);
 		}
-	})
+	})	
 });
 
 module.exports = router;
