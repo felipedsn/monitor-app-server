a79cbd66b0344f6eb503518cc3c435b86c8d9aa6
Felipe Nogueira <fadsnogueira@gmail.com>
2017-11-20 22:34:01 -0200

    Implement house people counter

--- CONSTANTS.js
+++ CONSTANTS.js
@@ -1,3 +1,4 @@
 module.exports = {
     EVENT_VERIFIER_INTERVAL: 1800000, // 30 minutes
+    HOME_ENTRANCE_INTERVAL: 120 // 2 minutes
 }
--- routes/sensors.js
+++ routes/sensors.js
@@ -1,6 +1,56 @@
 var express = require('express');
 var router = express.Router();
 var db = require("../db");
+var CONSTANTS = require("./CONSTANTS");
+var fs = require('fs');
+var xml2js = require('xml2js');
+var parser = new xml2js.Parser({explicitArray : false});
+var xmlFile = __dirname + "/../states.xml";
+
+var Sensors = db.Mongoose.model('sensors', db.SensorSchema, 'sensors');
+
+function getLastMainRegistry() {
+	Sensors.findOne().or([
+          { $and: [{type: "door"}, {info: "main"}] },
+          { $and: [{type: "passage"}, {info: "main"}] }
+      ]).sort({createdDate: -1}).exec(function(err, post){
+    	return post;
+	});
+}
+
+function itsBeenLessThanInterval(lastEntryDate) {
+	var dateNow = moment();
+	var entryDate = moment(lastEntryDate);
+	return moment.duration(dateNow.diff(entryDate)).asSeconds() <= CONSTANTS.HOME_ENTRANCE_INTERVAL;
+}
+
+function updateNumberOfPeople(update) {
+	fs.readFile(xmlFile, "utf-8", function (err, text) {
+        if (err) {
+        	console.log("Error reading XML states file: " + err.message);
+			return;
+        } else {
+            parser.parseString(text, function (err, result) {
+            	if (err) {
+            		console.log("Error parsing XML states file: " + err.message);
+					return;
+            	}
+                result.states.numberOfPeople += update;
+
+                var builder = new xml2js.Builder();
+        		var xmlText = builder.buildObject(result);
+        		fs.writeFile(xmlFile, xmlText, function(err, data){
+            		if (err) {
+            			console.log("Error updating number of people in XML states file: " + err.message);
+            			return;
+            		}
+            		console.log("Successfully updated number of people in XML states file!");
+       			})
+       			return;
+            });
+        }
+   });
+}
 
 /* Insert sensor notification in database */
 router.post('/', function(req, res) {
@@ -9,18 +59,31 @@ router.post('/', function(req, res) {
 	var type =  req.body.type;
 	var info = req.body.info;
 
-	//Validate values
 	if(type == "door") {
 		if (info !== "main" && info !== "medicine") {
 			res.status(400);
 			res.send({ error: "Invalid info for the type door"});
 			return;
+		} else if (info == "main") {
+			var lastMainRegistry = getLastMainRegistry();
+			if (lastMainRegistry !== null && lastMainRegistry.type == "passage" && itsBeenLessThanInterval(lastMainRegistry.createdDate)) {
+				//Left home, so update is minus one
+				console.log("Someone left the house.");
+				updateNumberOfPeople(-1);
+			}
 		}
 	} else if (type == "passage") {
 		if (info !== "main" && info !== "hall") {
 			res.status(400);
 			res.send({ error: "Invalid info for the type passage"});
 			return;
+		} else if (info == "main") {
+			var lastMainRegistry = getLastMainRegistry();
+			if (lastMainRegistry !== null && lastMainRegistry.type == "door" && itsBeenLessThanInterval(lastMainRegistry.createdDate)) {
+				//Entered home, so update is plus one
+				console.log("Someone entered the house.");
+				updateNumberOfPeople(1)
+			}
 		}
 	} else if (type == "moviment") {
 		if (info !== "moving" && info !== "fall") {
@@ -30,7 +93,6 @@ router.post('/', function(req, res) {
 		}
 	}
 
-	var Sensors = db.Mongoose.model('sensors', db.SensorSchema, 'sensors');
 	var sensor = new Sensors({ type: type, info: info });
 	sensor.save(function (err) {
 		if(err) {
