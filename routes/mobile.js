32d4cb3f70301748039166ec568c1d8f3616b8d3
Nogueira <fadsnogueira@gmail.com>
2018-09-10 00:34:20 -0300

    Use repositories in mobile route

--- routes/mobile.js
+++ routes/mobile.js
@@ -1,86 +1,72 @@
 var express = require('express');
 var router = express.Router();
-var db = require("../db");
 var moment = require('moment');
-var fs = require('fs');
-var xml2js = require('xml2js');
-var parser = new xml2js.Parser({explicitArray : false});
-var xmlFile = __dirname + "/../states.xml";
-
-var Sensors = db.Mongoose.model('sensors', db.SensorSchema, 'sensors');
+var statesRepository = require('../repositories/statesRepository')
+var sensorsRepository = require('../repositories/sensorsRepository')
 
 router.get('/home-status', function(req, res) {
 	console.log("Receive GET to /home-status");
 
-	fs.readFile(xmlFile, "utf-8", function (err, text) {
-        if (err) {
-        	console.log("Error reading XML states file: " + err.message);
-			res.status(400);
-			res.send(err);
-        } else {
-            parser.parseString(text, function (err, result) {
-            	if (err) {
-            		console.log("Error parsing XML states file: " + err.message);
-					res.status(400);
-					res.send(err);
-            	}
-                var numberOfPeople = result.states.numberOfPeople;
-                if(numberOfPeople == 1) {
-                	var status = res.__("STATUS_ALONE");
-                } else if (numberOfPeople <= 0) {
-                	var status = res.__("STATUS_NOT_HOME");
-                } else {
-                	var status = res.__n("STATUS_WITH_VISITOR", "STATUS_WITH_VISITORS", numberOfPeople-1);
-                }
-                res.status(200);
-                res.send({  status: status });
-            });
-        }
-   });
+  statesRepository.respondStates(res, function(result) {
+    var numberOfPeople = result.states.numberOfPeople;
+    if(numberOfPeople == 1) {
+      var status = res.__("STATUS_ALONE");
+    } else if (numberOfPeople <= 0) {
+      var status = res.__("STATUS_NOT_HOME");
+    } else {
+      var status = res.__n("STATUS_WITH_VISITOR", "STATUS_WITH_VISITORS", numberOfPeople-1);
+    }
+    res.status(200);
+    res.send({  status: status });
+  });
 });
 
 router.get('/last-movement', function(req, res) {
 	console.log("Receive GET to /last-movement");
 
-	Sensors.findOne().or([
-          { $and: [{type: "movement"}, {info: "moving"}] },
-          { $and: [{type: "passage"}, {info: "hall"}] }
-      ]).sort({createdDate: -1}).exec(function(err, post){
-      	if (post !== null) {
-      		res.send({ time: moment(post.createdDate).format('MMM DD, YYYY HH:mm') });
-      	} else {
-      		res.send({ time: res.__("NO_LAST_MOVEMENT") });
-      	}
-    	
-	});
+  sensorsRepository.getLastMovementRegistry(function(err, post) {
+    if(err) {
+      console.log("Some error happened when returning last movement: " + err.message);
+      res.status(400);
+      res.send(err);
+    } else if (post !== null) {
+      res.send({ time: moment(post.createdDate).format('MMM DD, YYYY HH:mm') });
+    } else {
+      res.send({ time: res.__("NO_LAST_MOVEMENT") });
+    }
+  });
 });
 
 router.get('/last-medicine', function(req, res) {
 	console.log("Receive GET to /last-medicine");
 
-	Sensors.findOne({ $and: [{type: "door"}, {info: "medicine"}] })
-			.sort({createdDate: -1}).exec(function(err, post){
-      	if (post !== null) {
-      		res.send({ time: moment(post.createdDate).format('MMM DD, YYYY HH:mm') });
-      	} else {
-      		res.send({ time: res.__("NO_MEDICINE_DETECTED") });
-      	}
-    	
-	});
+  sensorsRepository.getLastMedicineRegistry(function(err, post) {
+    if(err) {
+      console.log("Some error happened when returning last medicine: " + err.message);
+      res.status(400);
+      res.send(err);
+    } else if (post !== null) {
+      res.send({ time: moment(post.createdDate).format('MMM DD, YYYY HH:mm') });
+    } else {
+      res.send({ time: res.__("NO_MEDICINE_DETECTED") });
+    }
+  });
 });
 
 router.get('/last-fall', function(req, res) {
 	console.log("Receive GET to /last-fall");
 
-	Sensors.findOne({ $and: [{type: "moviment"}, {info: "fall"}] })
-			.sort({createdDate: -1}).exec(function(err, post){
-      	if (post !== null) {
-      		res.send({ time: moment(post.createdDate).format('MMM DD, YYYY HH:mm') });
-      	} else {
-      		res.send({ time: res.__("NO_FALL_DETECTED") });
-      	}
-    	
-	});
+  sensorsRepository.getLastFallRegistry(function(err, post) {
+    if(err) {
+      console.log("Some error happened when returning last fall: " + err.message);
+      res.status(400);
+      res.send(err);
+    } else if (post !== null) {
+      res.send({ time: moment(post.createdDate).format('MMM DD, YYYY HH:mm') });
+    } else {
+      res.send({ time: res.__("NO_FALL_DETECTED") });
+    }
+  });
 });
 
 module.exports = router;
