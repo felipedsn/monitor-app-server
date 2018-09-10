8b469623b2ed89ee0d4264b7cb34d0921d26be35
Nogueira <fadsnogueira@gmail.com>
2018-09-10 00:40:57 -0300

    Implement event verifier

--- eventVerifier.js
+++ eventVerifier.js
@@ -1,102 +1,93 @@
-var CONSTANTS = require("./CONSTANTS");
-var db = require("./db");  
 var moment = require('moment');
-var fs = require('fs');
-var xml2js = require('xml2js');
-var parser = new xml2js.Parser({explicitArray : false});
-var xmlFile = __dirname + "/../states.xml";
-
-var Sensors = db.Mongoose.model('sensors', db.SensorSchema, 'sensors');
+var CONSTANTS = require("./CONSTANTS");
+var statesRepository = require('./repositories/statesRepository')
+var sensorsRepository = require('./repositories/sensorsRepository')
+var fcm = require('./firebase/fcm')
 
 var isHome, sleepTimeFromInMinutes, sleepTimeToInMinutes, medicineTimeInMinutes;
 
-function updateStates() {
-	fs.readFile(xmlFile, "utf-8", function (err, text) {
-        if (err) {
-        	console.log("Error reading XML states file: " + err.message);
-			res.status(400);
-			res.send(err);
+function updateStates(callback) {
+	statesRepository.getStates(function(result) {
+		var numberOfPeople = Number(result.states.numberOfPeople);
+        var medicineHour = Number(result.states.medicineTime.hour);
+    	var medicineMinute = Number(result.states.medicineTime.minute);
+        var sleepHourFrom = Number(result.states.sleepTime.from.hour);
+        var sleepMinuteFrom = Number(result.states.sleepTime.from.minute);
+        var sleepHourTo = Number(result.states.sleepTime.to.hour);
+        var sleepMinuteTo = Number(result.states.sleepTime.to.minute);
+
+        //Set isHome
+        if(numberOfPeople >= 1) {
+        	isHome = true;
         } else {
-            parser.parseString(text, function (err, result) {
-            	if (err) {
-            		console.log("Error parsing XML states file: " + err.message);
-            	}
-                var numberOfPeople = result.states.numberOfPeople;
-                var medicineHour = result.states.medicineTime.hour;
-            	var medicineMinute = result.states.medicineTime.minute;
-                var sleepHourFrom = result.states.sleepTime.from.hour;
-                var sleepMinuteFrom = result.states.sleepTime.from.minute;
-                var sleepHourTo = result.states.sleepTime.to.hour;
-                var sleepMinuteTo = result.states.sleepTime.to.minute;
-
-                //Set isHome
-                if(numberOfPeople >= 1) {
-                	isHome = true;
-                } else {
-                	isHome = false;
-                }
-
-                //Set sleepTime in minutes
-                sleepTimeFromInMinutes = (sleepHourFrom * 60) + selepMinuteFrom
-                sleepTimeToInMinutes = (sleepHourTo * 60) + sleepMinuteTo
-
-                //Set medicineTime in minutes
-                medicineTimeInMinutes = (medicineHour * 60) + medicineMinute
-            });
+        	isHome = false;
         }
-   });
+
+        //Set sleepTime in minutes
+        sleepTimeFromInMinutes = (sleepHourFrom * 60) + sleepMinuteFrom
+        sleepTimeToInMinutes = (sleepHourTo * 60) + sleepMinuteTo
+
+        //Set medicineTime in minutes
+        medicineTimeInMinutes = (medicineHour * 60) + medicineMinute
+
+        callback();
+	});
 }
 
 function verifyIdleness() {
-	//If passed X time since last movement detection and is not in the sleeping time, then send push noticiation Not moving
-
+	console.log("Verifying idleness");
+	//If passed X time since last movement detection and is not in the sleeping time, then send push notification Idleness
 	if(!isBetweenSleepTime()) {
-		Sensors.findOne().or([
-	          { $and: [{type: "movement"}, {info: "moving"}] },
-	          { $and: [{type: "passage"}, {info: "hall"}] }
-	      ]).sort({createdDate: -1}).exec(function(err, post){
-	      	if (post !== null) {
-	      		if(itsBeenLessThanInterval(post.createdDate, CONSTANTS.IDLENESS_INTERVAL)) {
-	      			//TODO Push Notification
+		sensorsRepository.getLastMovementRegistry(function(err, post) {
+			if(err) {
+				console.log("Some error happened when verifying idleness: " + err.message);
+			} else if (post !== null) {
+	      		if(itsBeenMoreThanInterval(post.createdDate, CONSTANTS.IDLENESS_INTERVAL)) {
+	      			console.log("Idlness detected, sending push notification");
+	      			//TODO send time by parameter
+	      			fcm.sendPushNotification(__("PUSH_NOTIFICATION_TITLE_ATTENTION"), __("PUSH_NOTIFICATION_BODY_IDLNESS"));
 	      		}
-	      	} 
+		    } 
 		});
-  	}
+	} else {
+		console.log("Monitored is in sleep time ");
+	}
 }
 
 function verifyMedicineTime() {
+	console.log("Verifying medicine time");
 	//If passed X time since the configured time to take medicine and no medicine event happened, then send push notification Didn't take medicine
-
-	Sensors.findOne({ $and: [{type: "door"}, {info: "medicine"}] })
-			.sort({createdDate: -1}).exec(function(err, post){
-      	if (post !== null) {
-      		var nowInMinutes = moment.duration().asMinutes();
+	sensorsRepository.getLastMedicineRegistry(function(err, post) {
+		if(err) {
+				console.log("Some error happened when verifying medicine time: " + err.message);
+		} else if (post !== null) {
+      		var dateNow = moment();
+			var nowInMinutes = (dateNow.hour()*60) + dateNow.minute();
       		var diffNowInSeconds = (nowInMinutes - medicineTimeInMinutes) * 60
       		if(moment(post.createdDate).isSame(moment(), 'day')) {
-	      		var lastMedicineInMinutes = moment(post.createdDate).duration().asMinutes();
+	      		var lastMedicine = moment(post.createdDate);
+	      		var lastMedicineInMinutes = (lastMedicine.hour()*60) + lastMedicine.minute();
 	      		var diffLastMedicineInSeconds = (medicineTimeInMinutes - lastMedicineInMinutes) * 60
 
 	      		if(diffLastMedicineInSeconds >= CONSTANTS.PRE_MEDICINE_INTERVAL) {
 	      			if(diffNowInSeconds >= CONSTANTS.POST_MEDICINE_INTERVAL) {
-	      				//TODO Push Notification
+	      				console.log("No medicine detected, sending push notification");
+	      				fcm.sendPushNotification(__("PUSH_NOTIFICATION_TITLE_ATTENTION"), __("PUSH_NOTIFICATION_BODY_NO_MEDICINE"));
 	      			}
 	      		}
       		} else {
       			if(diffNowInSeconds >= CONSTANTS.POST_MEDICINE_INTERVAL) {
-      				//TODO Push Notification
+      				console.log("No medicine detected, sending push notification");
+      				fcm.sendPushNotification(__("PUSH_NOTIFICATION_TITLE_ATTENTION"), __("PUSH_NOTIFICATION_BODY_NO_MEDICINE"));
       			}
-
       		}
       	} 
 	});
 }
 
-function sendPushNotification() {
-	
-}
-
 function isBetweenSleepTime() {
-	var nowInMinutes = moment.duration().asMinutes();
+	var dateNow = moment();
+	var nowInMinutes = (dateNow.hour()*60) + dateNow.minute();
 	if(sleepTimeFromInMinutes >= sleepTimeToInMinutes) {
 		if(nowInMinutes >= sleepTimeFromInMinutes) {
 			return true;
@@ -121,17 +112,18 @@ function itsBeenMoreThanInterval(lastEntryDate, interval) {
 }
 
 function verifyEvents() {
-	updateStates();
-	verifyMedicineTime();
-	if(isHome) {
-		verifyIdleness();
-	}
-
-	console.log("OIEEE");
+	updateStates(function() {
+		console.log("Event verifier triggered");
+		verifyMedicineTime();
+		if(isHome) {
+			console.log("Monitored is in home");
+			verifyIdleness();
+		}
+	});
 }
 
 module.exports = function(){
 	setInterval(function(){ 
 		verifyEvents(); 
-	}, CONSTANTS.EVENT_VERIFIER_INTERVAL);
+	}, (CONSTANTS.EVENT_VERIFIER_INTERVAL*1000));
 }
