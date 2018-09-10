f64f9f58b6344a1ab1d3ea629f2bd60b72c29d8f
Nogueira <fadsnogueira@gmail.com>
2018-09-10 00:13:46 -0300

    Allow global call to i18n

--- i18n.js
+++ i18n.js
@@ -1,21 +1,23 @@
 var i18n = require('i18n');
 
 i18n.configure({
-  // setup some locales - other locales default to en silently
-  locales:['en'],
+	// setup some locales - other locales default to en silently
+	locales:['en'],
 
-  // where to store json files - defaults to './locales' relative to modules directory
-  directory: __dirname + '/locales',
-  
-  defaultLocale: 'en'
+	// where to store json files - defaults to './locales' relative to modules directory
+	directory: __dirname + '/locales',
+
+	defaultLocale: 'en',
+
+	register: global
 });
 
 module.exports = function(req, res, next) {
 
-  i18n.init(req, res);
-  res.locals.__ = res.__;
+	i18n.init(req, res);
+	res.locals.__ = res.__;
 
-  var current_locale = i18n.getLocale();
+	var current_locale = i18n.getLocale();
 
-  return next();
+	return next();
 };
