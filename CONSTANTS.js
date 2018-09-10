493019e2e2761226630ea79a581354fcfc9593be
Nogueira <fadsnogueira@gmail.com>
2018-09-10 00:14:59 -0300

    Change event verifier interval to seconds

--- CONSTANTS.js
+++ CONSTANTS.js
@@ -1,5 +1,5 @@
 module.exports = {
-    EVENT_VERIFIER_INTERVAL: 1800000, // 30 minutes in milliseconds
+    EVENT_VERIFIER_INTERVAL: 1800, // 30 minutes in seconds
     IDLENESS_INTERVAL: 3600, // 1 hour in seconds
     POST_MEDICINE_INTERVAL: 3600, // 1 hour in seconds
     PRE_MEDICINE_INTERVAL: 21600, // 6 hours in seconds
